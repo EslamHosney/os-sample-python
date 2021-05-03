3d0ed919f73e77560a3c19eed63c51e39ffdad1b
Eslam Hosney <eslamsamymeg@gmail.com>
2021-05-03 14:12:14 +0200

    Islam Print statement

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,9 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    #Islam start
+    print ("Print to Screen Islam Awesome")
+    #Islam Stop
     return "Hello World Eslam!"
 
 if __name__ == "__main__":
