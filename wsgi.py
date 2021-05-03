3b6cdfeb47a6ae3796bd7d61e8f4984db1a56acc
Eslam Hosney <eslamsamymeg@gmail.com>
2021-05-03 14:17:51 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,9 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     #Islam start
-    input ("Enter any char")
-    print ("Print to Screen Islam Awesome")
+    #input ("Enter any char")
+    while True:
+        print ("Print to Screen Islam Awesome")
     #Islam Stop
     return "Hello World Eslam Added!"
 
